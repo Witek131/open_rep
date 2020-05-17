4d6bd9c56798c1e905d642f8277bf33fba3e9cba
kprmk <kpriemko@gmail.com>
2020-05-17 09:47:54 +0300

    A Chore_bot

---
+++ Chore_bot/script.js
@@ -0,0 +1,4 @@
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg"
+const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg"
+const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg"
+const closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg"
